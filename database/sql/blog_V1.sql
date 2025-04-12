7af8d2a6ad97f721f0e742388b60608fb144efa0
developer-jiheui <developer.jiheuilee@gmail.com>
2025-04-11 17:40:23 -0700

    comment table added

--- database/sql/blog_V1.sql
+++ database/sql/blog_V1.sql
@@ -11,17 +11,16 @@ DROP TABLE IF EXISTS USER_T;
 
 -- user table
 CREATE TABLE USER_T (
-                        USER_NO INT NOT NULL AUTO_INCREMENT,
+                        USER_ID INT NOT NULL AUTO_INCREMENT,
                         EMAIL VARCHAR(100) NOT NULL UNIQUE,
+                        AVATAR VARCHAR(100),
                         PW VARCHAR(64),
                         USER_TYPE INT,# admin 0, common user 1
                         FIRST_NAME VARCHAR(100),
                         LAST_NAME VARCHAR(100),
-                        AGREED_EVENT INT,
                         REGISTER_TYPE INT, #if we want to add github or google register api
-                        PW_MODIFY_DT DATETIME,
                         REGISTER_DT DATETIME,
-                        LOCATION VARCHAR(100),
+                        ADDRESS VARCHAR(100),
                         PHONE_NUM VARCHAR(20),
                         BIO VARCHAR(300),
                         JOB_TITLE VARCHAR(100),
@@ -29,5 +28,22 @@ CREATE TABLE USER_T (
                         INSTAGRAM_URL VARCHAR(100),
                         LINKEDIN_URL VARCHAR(100),
                         GITHUB_URL VARCHAR(100),
-                        PRIMARY KEY (USER_NO)
+                        PRIMARY KEY (USER_ID)
+);
+
+CREATE TABLE COMMENT_T (
+                           COMMENT_NO INT NOT NULL,
+                           CONTENTS VARCHAR(4000) NOT NULL,
+                           CREATE_DT TIMESTAMP NULL,
+                           STATE TINYINT,                    -- 0: deleted, 1: normal, 2: edited
+                           DEPTH TINYINT,                    -- 0: original comment, 1: reply, 2: sub-reply
+                           GROUP_NO INT,                     -- All replies to the same original comment share the same GROUP_NO
+                           USER_NO INT,
+                           BLOG_NO INT,
+                           PARENT_NO INT,
+                           PRIMARY KEY (COMMENT_NO),
+                           CONSTRAINT FK_COMMENT_USER FOREIGN KEY (USER_NO)
+                               REFERENCES USER_T(USER_NO) ON DELETE CASCADE,
+                           CONSTRAINT FK_COMMENT_BLOG FOREIGN KEY (BLOG_NO)
+                               REFERENCES BLOG_T(BLOG_NO) ON DELETE CASCADE
 );
