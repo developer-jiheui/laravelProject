3edd4fae23fab6c4a2e9cda5f7690a551c456bfb
Null <nullv@student.douglascollege.ca>
2025-04-11 20:49:23 -0700

    Made portfolio page display items from portfolio database

--- resources/views/pages/portfolio.blade.php
+++ resources/views/pages/portfolio.blade.php
@@ -13,18 +13,6 @@
                 <li class="filter-item">
                     <button class="active" data-filter-btn>All</button>
                 </li>
-
-                <!--<li class="filter-item">
-                    <button data-filter-btn>Web design</button>
-                </li>
-
-                <li class="filter-item">
-                    <button data-filter-btn>Applications</button>
-                </li>
-
-                <li class="filter-item">
-                    <button data-filter-btn>Web development</button>
-                </li>-->
                 @foreach (\App\Models\Portfolio::categories() as $category)
                 <li class="filter-item">
                     <button data-filter-btn>{{$category}}</button>
@@ -64,169 +52,24 @@
             <!--  TODO MAKE THE PROJECT PART LIKE A BLOG POST-->
 
             <ul class="project-list">
-
-                <li class="project-item  active" data-filter-item data-category="web development">
-                    <a href="#">
-
-                        <figure class="project-img">
+                @foreach (\App\Models\Portfolio::all()->toArray() as $item)
+                <li class="project-item active" data-filter-item data-category="{{$item['CATEGORY']}}">
+                    <a href=#> <!-- TODO MAKE THE LINK DO SOMETHING -->
+                         <figure class="project-img">
                             <div class="project-item-icon-box">
                                 <button select-project><ion-icon name="eye-outline"></ion-icon></button>
                             </div>
 
-                            <img src="./assets/images/projects/project_1.png" alt="jwitter" loading="lazy">
+                            <img src="{{$item['IMAGE_URL']}}" alt loading="lazy">
                         </figure>
 
-                        <h3 class="project-title">프로젝트명</h3>
+                        <h3 class="project-title">{{$item['TITLE']}}</h3>
 
-                        <p class="project-category">Web development</p>
-
+                        <p class="project-category">{{$item['CATEGORY']}}</p>
                     </a>
+                </li> <!-- TODO likes -->
+                @endforeach
 
-                </li>
-
-                <li class="project-item  active" data-filter-item data-category="web development">
-                    <a href="#">
-
-                        <figure class="project-img">
-                            <div class="project-item-icon-box">
-                                <ion-icon name="eye-outline"></ion-icon>
-                            </div>
-
-                            <img src="./assets/images/projects/dscWebsite.png" alt="dscwebsite" loading="lazy">
-                        </figure>
-
-                        <h3 class="project-title">프로젝트명</h3>
-
-                        <p class="project-category">Web development</p>
-
-                    </a>
-                </li>
-
-                <li class="project-item  active" data-filter-item data-category="web design">
-                    <a href="#">
-
-                        <figure class="project-img">
-                            <div class="project-item-icon-box">
-                                <ion-icon name="eye-outline"></ion-icon>
-                            </div>
-
-                            <img src="./assets/images/projects/project-3.png" alt="fundo" loading="lazy">
-                        </figure>
-
-                        <h3 class="project-title">프로젝트명</h3>
-
-                        <p class="project-category">Web design</p>
-
-                    </a>
-                </li>
-
-                <li class="project-item  active" data-filter-item data-category="applications">
-                    <a href="#">
-
-                        <figure class="project-img">
-                            <div class="project-item-icon-box">
-                                <ion-icon name="eye-outline"></ion-icon>
-                            </div>
-
-                            <img src="./assets/images/projects/garagesale.PNG" alt="garagesale" loading="lazy">
-                        </figure>
-
-                        <h3 class="project-title">프로젝트명</h3>
-
-                        <p class="project-category">Applications</p>
-
-                    </a>
-                </li>
-
-                <li class="project-item  active" data-filter-item data-category="web design">
-                    <a href="#">
-
-                        <figure class="project-img">
-                            <div class="project-item-icon-box">
-                                <ion-icon name="eye-outline"></ion-icon>
-                            </div>
-
-                            <img src="./assets/images/projects/project-5.png" alt="dsc" loading="lazy">
-                        </figure>
-
-                        <h3 class="project-title">프로젝트명</h3>
-
-                        <p class="project-category">Web design</p>
-
-                    </a>
-                </li>
-
-                <li class="project-item  active" data-filter-item data-category="web design">
-                    <a href="#">
-
-                        <figure class="project-img">
-                            <div class="project-item-icon-box">
-                                <ion-icon name="eye-outline"></ion-icon>
-                            </div>
-
-                            <img src="./assets/images/projects/project-6.png" alt="garage_sale" loading="lazy">
-                        </figure>
-
-                        <h3 class="project-title">프로젝트명</h3>
-
-                        <p class="project-category">Web design</p>
-
-                    </a>
-                </li>
-
-                <li class="project-item  active" data-filter-item data-category="web development">
-                    <a href="#">
-
-                        <figure class="project-img">
-                            <div class="project-item-icon-box">
-                                <ion-icon name="eye-outline"></ion-icon>
-                            </div>
-
-                            <img src="./assets/images/project-7.png" alt="summary" loading="lazy">
-                        </figure>
-
-                        <h3 class="project-title">프로젝트명</h3>
-
-                        <p class="project-category">Web development</p>
-
-                    </a>
-                </li>
-
-                <li class="project-item  active" data-filter-item data-category="applications">
-                    <a href="#">
-
-                        <figure class="project-img">
-                            <div class="project-item-icon-box">
-                                <ion-icon name="eye-outline"></ion-icon>
-                            </div>
-
-                            <img src="./assets/images/project-8.jpg" alt="task manager" loading="lazy">
-                        </figure>
-
-                        <h3 class="project-title">프로젝트명</h3>
-
-                        <p class="project-category">Applications</p>
-
-                    </a>
-                </li>
-
-                <li class="project-item  active" data-filter-item data-category="web development">
-                    <a href="#">
-
-                        <figure class="project-img">
-                            <div class="project-item-icon-box">
-                                <ion-icon name="eye-outline"></ion-icon>
-                            </div>
-
-                            <img src="./assets/images/project-9.png" alt="arrival" loading="lazy">
-                        </figure>
-
-                        <h3 class="project-title">프로젝트명</h3>
-
-                        <p class="project-category">Web development</p>
-
-                    </a>
-                </li>
 
             </ul>
 
