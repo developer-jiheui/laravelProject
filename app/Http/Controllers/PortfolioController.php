ddf6c2c24c7c53e636e226a0e2027042257a2372
George <georgejw555@gmail.com>
2025-04-21 01:52:25 -0700

    Screwed up with merging.  Trying to revert back to branch without dev merge...

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -50,15 +50,4 @@ class PortfolioController extends Controller {
         DB::update('UPDATE portfolio SET like_count = (SELECT COUNT(*) FROM likes WHERE portfolio_id=?) WHERE portfolio_id = ?',[$_GET['id'],$_GET['id']]);
         return view('pages.portfolio');
     }
-    public function like() {
-        $existingLike = DB::scalar('SELECT COUNT(*) FROM likes WHERE portfolio_id = ? AND user_id=? LIMIT 1',[$_GET['id'],Auth::user()->USER_ID]);
-        if ($existingLike) {
-            DB::delete('DELETE FROM  likes WHERE user_id=? AND portfolio_id=?',[Auth::user()->USER_ID,$_GET['id']]);
-        }
-        else {
-            DB::insert('INSERT INTO likes (user_id, portfolio_id) values (?, ?)',[Auth::user()->USER_ID,$_GET['id']]);
-        }
-        DB::update('UPDATE portfolio SET like_count = (SELECT COUNT(*) FROM likes WHERE portfolio_id=?) WHERE portfolio_id = ?',[$_GET['id'],$_GET['id']]);
-        return view('pages.portfolio');
-    }
 }
